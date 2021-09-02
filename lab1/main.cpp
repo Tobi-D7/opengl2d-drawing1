f991112024c260407741a1095cf67a548e38700a
Tobi <tobid7@outlook.de>
2021-09-02 22:37:29 +0200

    Update main.cpp

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char **argv){
              printf("6");
              
              printf("7");
-            // pglSwapBuffers();
+             pglSwapBuffers();
         }
         pglExit();
 	return 0;
